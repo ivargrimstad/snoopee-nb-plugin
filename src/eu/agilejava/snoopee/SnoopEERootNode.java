716f93dd7ea42f695bb2802c2bb9c00d85aa156b
Ivar Grimstad <ivar.grimstad@gmail.com>
2017-02-08 21:22:55 +0100

    Regenerate project on 8.2

--- src/eu/agilejava/snoopee/SnoopEERootNode.java
+++ src/eu/agilejava/snoopee/SnoopEERootNode.java
@@ -1,8 +1,11 @@
 package eu.agilejava.snoopee;
 
+import javax.swing.Action;
 import org.netbeans.api.core.ide.ServicesTabNodeRegistration;
+import org.openide.actions.NewAction;
 import org.openide.nodes.AbstractNode;
 import static org.openide.nodes.Children.LEAF;
+import org.openide.util.actions.SystemAction;
 
 /**
  *
@@ -14,8 +17,17 @@ import static org.openide.nodes.Children.LEAF;
         iconResource = "eu/agilejava/snoopee/snoopee-icon.png")
 public class SnoopEERootNode extends AbstractNode {
 
+    private String snoopeeServiceRoot;
+    
     public SnoopEERootNode() {
         super(LEAF);
     }
 
+    @Override
+    public Action getPreferredAction() {
+        return SystemAction.get(NewAction.class);
+    }
+
+    
+    
 }
